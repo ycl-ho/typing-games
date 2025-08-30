d45b5e3f4b98f34cd2aac1a9d60b529787588826
Louie <yu.louis.ho@gmail.com>
2025-08-29 21:40:11 -0500

    add countdown timer

--- cs50-lua/main.lua
+++ cs50-lua/main.lua
@@ -5,8 +5,12 @@
 	yu.louis.ho@gmail.com
 ]]
 
+Timer = require 'lib/knife/knife.timer'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+
+local currentTime = 60
 
 local font = love.graphics.newFont('fonts/font.ttf', 64)
 
@@ -14,6 +18,10 @@ function love.load()
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 	love.graphics.setFont(font)
+
+	Timer.every(1, function()
+		currentTime = currentTime - 1
+	end)
 end
 
 function love.keypressed(key)
@@ -23,27 +31,28 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-
+	Timer.update(dt)
 end
 
 function love.draw()
 	
+	local fullString = 'Supercalifragilistic'
+	local halfString = 'Supercali'
+	
 	-- draw the current goal word in yellow
 	love.graphics.setColor(1, 1, 0, 1)
-	love.graphics.printf('CS50', 0, WINDOW_HEIGHT / 2 - 32, WINDOW_WIDTH, 'center')
+	love.graphics.printf(fullString, 0, WINDOW_HEIGHT / 2 - 32, WINDOW_WIDTH, 'center')
 	love.graphics.setColor(1, 1, 1, 1)
 
 	-- draw the progress of the word we're typing in white
-	local fullString = 'CS50'
-	local halfString = 'CS5'
 	love.graphics.printf(halfString, 0, WINDOW_HEIGHT / 2 + 16, 
 		WINDOW_WIDTH - font:getWidth(fullString:sub(halfString:len() + 1, fullString:len())), 'center')	
 
 	-- draw the timer in the top-left
-	love.graphics.print('60')
+	love.graphics.print(tostring(currentTime))
 	
 	-- draw the score in the top-right
 	love.graphics.printf('24', 0, 0, WINDOW_WIDTH, 'right')
 	
-	-- 45:07
+	-- 
 end
